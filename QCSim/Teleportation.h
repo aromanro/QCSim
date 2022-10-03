95a8c3ddf97bbfe8b5265898dc05d00bb42000d5
Adrian Roman <aroman_ro@yahoo.com>
2022-10-03 17:10:57 +0300

    Derived a class for quantum teleportation that will contain a particular implementation (one from the book 'Quantum Computation and Quantum Information')

--- QCSim/Teleportation.h
+++ QCSim/Teleportation.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "QuantumAlgorithm.h"
+#include "QuantumGate.h"
 
 namespace Teleportation
 {
@@ -12,6 +13,24 @@ namespace Teleportation
             : QC::QuantumAlgorithm<VectorClass, MatrixClass>(N, addseed)
         {
         }
+    };
+
+
+    template<class VectorClass = Eigen::VectorXcd, class MatrixClass = Eigen::MatrixXcd> class QuantumTeleportationRealization : QuantumTeleportation < VectorClass, MatrixClass)
+    {
+    public:
+        QuantumTeleportationRealization(unsigned int N = 3, int addseed = 0)
+            : QuantumTeleportation(N, addseed)
+        {
+        }
+
+    protected:
+        // the initial state to be teleported can be set directly or set up from something simple using some gate (hadamard, phase shift)
+        // needed to create the Bell states
+        QC::HadamardGate hadamard;
+        QC::CNOTGate cnot; // also needed on Alice side
+        // needed on receiving side:
+        QC::ControlledZGate cz;
     };
 
 }
