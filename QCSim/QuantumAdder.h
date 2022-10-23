46961de3de44e41cadb4593c7e7bee1e60c972eb
Adrian Roman <aroman_ro@yahoo.com>
2022-10-23 23:52:52 +0300

    Added implementation for a full quantum adder

--- QCSim/QuantumAdder.h
+++ QCSim/QuantumAdder.h
@@ -7,16 +7,17 @@ namespace QC {
 
 	// to be used to implement a full quantum adder
 	// not tested yet
-	template<class VectorClass = Eigen::VectorXcd, class MatrixClass = Eigen::MatrixXcd> class TwoQubitsAdder : public QuantumSubAlgorithm<VectorClass, MatrixClass>
+	template<class VectorClass = Eigen::VectorXcd, class MatrixClass = Eigen::MatrixXcd> class TwoQubitsHalfAdder : public QuantumSubAlgorithm<VectorClass, MatrixClass>
 	{
 	public:
-		TwoQubitsAdder(unsigned int qubit1, unsigned int qubit2, unsigned int qubitaux)
+		TwoQubitsHalfAdder(unsigned int qubit1, unsigned int qubit2, unsigned int qubitaux)
 			: q1(qubit1), q2(qubit2), aux(qubitaux)
 		{
 		}
 
 		unsigned int Execute(QubitRegister<VectorClass, MatrixClass>& reg) const override
 		{
+			// TODO: check if the registry is big enough
 			reg.ApplyGate(toffoli, aux, q1, q2);
 			reg.ApplyGate(cnot, q2, q1);
 
@@ -25,6 +26,7 @@ namespace QC {
 
 	protected:
 		// q1, q2 - the qubits to be added, aux, the auxiliary that should be initialized to |0> and has the 'carry' functionality
+		// 'sum' ends up in q2, carry in aux
 		unsigned int q1;
 		unsigned int q2;
 		unsigned int aux;
@@ -33,4 +35,29 @@ namespace QC {
 		QC::CNOTGate cnot;
 	};
 
+	template<class VectorClass = Eigen::VectorXcd, class MatrixClass = Eigen::MatrixXcd> class TwoQubitsFullAdder : public QuantumHalfAdder<VectorClass, MatrixClass>
+	{
+	public:
+		TwoQubitsFullAdderAdder(unsigned int qubit1, unsigned int qubit2, unsigned int cin, unsigned int qubitaux)
+			: QuantumHalfAdder<VectorClass, MatrixClass>(qubit1, qubit2, qubitaux), ci(cin),
+			halfAdder(qubit2, cin, aux)
+		{
+		}
+
+		unsigned int Execute(QubitRegister<VectorClass, MatrixClass>& reg) const override
+		{
+			// TODO: check if the registry is big enough
+			QuantumHalfAdder<VectorClass, MatrixClass>::Execute(reg);
+
+			halfAdder.Execute(reg);
+			reg.ApplyGate(QuantumHalfAdder<VectorClass, MatrixClass>::cnot, qubit2, qubit1);
+
+			return 0;
+		}
+
+	protected:
+		unsigned int ci;
+
+		QuantumHalfAdder<VectorClass, MatrixClass> halfAdder;
+	};
 }
