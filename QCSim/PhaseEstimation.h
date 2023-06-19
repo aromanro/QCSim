e2d7e00209fa7aa53d96b694fbd47e682ae32569
Adrian Roman <aroman_ro@yahoo.com>
2023-06-19 15:43:31 +0300

    Added NQubitsControlledQuantumGate and using it I implemented a generic phase estimation subalgorithm (as opposed to the special one for Shor). Related with https://github.com/aromanro/QCSim/issues/3. The things I added today still need testing, though.

--- QCSim/PhaseEstimation.h
+++ QCSim/PhaseEstimation.h
@@ -2,7 +2,7 @@
 
 #include "QuantumFourierTransform.h"
 #include "Function.h"
-#include "NQubitsQuantumGate.h"
+#include "NQubitsControlledQuantumGate.h"
 
 namespace QC {
 
@@ -51,6 +51,82 @@ namespace QC {
 		QuantumFourierTransform<VectorClass, MatrixClass> fourier;
 		QC::Function<VectorClass, MatrixClass>& func;
 	};
+
+
+
+
+	template<class VectorClass = Eigen::VectorXcd, class MatrixClass = Eigen::MatrixXcd> class PhaseEstimation : public QuantumSubAlgorithm<VectorClass, MatrixClass>
+	{
+	public:
+		using RegisterClass = QubitRegister<VectorClass, MatrixClass>;
+
+		PhaseEstimation(const MatrixClass& op, unsigned int N = 7, unsigned int L = 3, int addseed = 0)
+			: fRegisterStartQubit(L), nrQubits(N), fourier(N, 0, L - 1), U(op)
+		{
+		}
+
+		unsigned int Execute(RegisterClass& reg) override
+		{
+			// apply hadamard over each qubit from the x-register
+			// reuse the hadamard gate from the fourier transform base class
+			for (unsigned int i = 0; i < fRegisterStartQubit; ++i)
+				reg.ApplyGate(fourier.hadamard, i);
+
+			
+			MatrixClass controlledGate = U;
+			for (unsigned int ctrlQubit = fRegisterStartQubit - 1; ctrlQubit > 0; --ctrlQubit)
+			{
+				NQubitsControlledQuantumGate<VectorClass, MatrixClass> UGate(nrQubits, controlledGate, fRegisterStartQubit, ctrlQubit);
+				
+				UGate.Execute(reg);
+
+				// double the controlled gate
+				controlledGate *= controlledGate;
+			}
+
+			{
+				NQubitsControlledQuantumGate<VectorClass, MatrixClass> UGate(nrQubits, controlledGate, fRegisterStartQubit, 0);
+
+				UGate.Execute(reg);
+			}
+
+			// it doesn't really matter if you measure the qubits from f and when you do after the above
+			// or if you measure them several times in a row
+			//QC::QuantumAlgorithm<VectorClass, MatrixClass>::Measure(fRegisterStartQubit, QC::QuantumAlgorithm<VectorClass, MatrixClass>::getNrQubits() - 1);
+
+			// then perform an inverse fourier transform
+			fourier.IQFT(reg);
+
+			// any of those following should do, but if one does not do the f register measurement above and here there is no full register measurement
+			// the f should be measured separately to find out its content
+
+			//return reg.Measure();
+			return reg.Measure(0, fRegisterStartQubit - 1);
+		}
+
+		unsigned int getFunctionStartQubit() const
+		{
+			return fRegisterStartQubit;
+		}
+
+		unsigned int getNrQubits() const
+		{
+			return nrQubits;
+		}
+
+		const MatrixClass& getU() const
+		{
+			return U;
+		}
+
+	protected:
+		unsigned int fRegisterStartQubit;
+		unsigned int nrQubits;
+
+		QuantumFourierTransform<VectorClass, MatrixClass> fourier;
+		MatrixClass U;
+	};
+
 
 }
 
