614d006a4902a287d8381d61a8502f5e43d689c2
Adrian Roman <aroman_ro@yahoo.com>
2023-06-22 13:22:22 +0300

    Split the quantum teleportation tests/examples function in two.

--- QCSim/Tests.cpp
+++ QCSim/Tests.cpp
@@ -147,63 +147,10 @@ bool GroverTests()
 	return res;
 }
 
-
-bool TeleportationTests()
+bool TeleportTests()
 {
-	std::cout << "\nTesting teleportation..." << std::endl;
-
-	Teleportation::QuantumTeleportationRealization<> qt;
-
-	for (int i = 0; i < 30; ++i)
-	{
-		if (dist_bool(gen))
-		{
-			qt.SetState(0, 1); // set the qubit to teleport to 'up'
-			const unsigned int state = qt.Execute();
-
-			std::cout << "Teleported 1, measured: " << state << std::endl;
-
-			if (state != 1) return false;
-		}
-		else
-		{
-			qt.SetState(1, 0); // set the qubit to teleport to 'down'
-			const unsigned int state = qt.Execute();
-
-			std::cout << "Teleported 0, measured: " << state << std::endl;
-
-			if (state != 0) return false;
-		}
-	}
-
-	std::cout << "Now testing teleporting a generic state:" << std::endl;
-
-	for (int i = 0; i < 16; ++i)
-	{
-		// generate and normalize
-		std::complex<double> alpha(dist_ampl(gen), dist_ampl(gen));
-		std::complex<double> beta(dist_ampl(gen), dist_ampl(gen));
-
-		const double norm = sqrt((alpha * std::conj(alpha) + beta * std::conj(beta)).real());
-		alpha /= norm;
-		beta /= norm;
-
-		qt.SetState(alpha, beta);
-		std::cout << "Teleporting " << alpha << "|0> + " << beta << "|1>";
-
-		const unsigned int classicalBits = qt.Teleport(i < 8 ? false : true); // also test sending explicitely the two classical bits for half the tests, although it should not make a difference
-		std::cout << " Measured values for the two qubits: " << classicalBits;
-
-		// how is the whole thing looking before Bob's measurement?
-		std::complex<double> receivedAlpha = qt.getBasisStateAmplitude(classicalBits);
-		std::complex<double> receivedBeta = qt.getBasisStateAmplitude(0x4 | classicalBits);
-		std::cout << "... Teleported state: " << receivedAlpha << "|0> + " << receivedBeta << "|1>" << std::endl;
-
-		if (!approxEqual(alpha, receivedAlpha) || !approxEqual(beta, receivedBeta)) return false;
-	}
-
-	std::cout << "Teleporting with teleport sub-algorithm:" << std::endl;\
-	QC::Teleport<> teleport;
+	std::cout << "Teleporting with teleport sub-algorithm:" << std::endl; \
+		QC::Teleport<> teleport;
 	QC::QubitRegister<> reg;
 
 	for (int i = 0; i < 30; ++i)
@@ -261,6 +208,64 @@ bool TeleportationTests()
 	}
 
 	return true;
+}
+
+
+bool TeleportationTests()
+{
+	std::cout << "\nTesting teleportation..." << std::endl;
+
+	Teleportation::QuantumTeleportationRealization<> qt;
+
+	for (int i = 0; i < 30; ++i)
+	{
+		if (dist_bool(gen))
+		{
+			qt.SetState(0, 1); // set the qubit to teleport to 'up'
+			const unsigned int state = qt.Execute();
+
+			std::cout << "Teleported 1, measured: " << state << std::endl;
+
+			if (state != 1) return false;
+		}
+		else
+		{
+			qt.SetState(1, 0); // set the qubit to teleport to 'down'
+			const unsigned int state = qt.Execute();
+
+			std::cout << "Teleported 0, measured: " << state << std::endl;
+
+			if (state != 0) return false;
+		}
+	}
+
+	std::cout << "Now testing teleporting a generic state:" << std::endl;
+
+	for (int i = 0; i < 16; ++i)
+	{
+		// generate and normalize
+		std::complex<double> alpha(dist_ampl(gen), dist_ampl(gen));
+		std::complex<double> beta(dist_ampl(gen), dist_ampl(gen));
+
+		const double norm = sqrt((alpha * std::conj(alpha) + beta * std::conj(beta)).real());
+		alpha /= norm;
+		beta /= norm;
+
+		qt.SetState(alpha, beta);
+		std::cout << "Teleporting " << alpha << "|0> + " << beta << "|1>";
+
+		const unsigned int classicalBits = qt.Teleport(i < 8 ? false : true); // also test sending explicitely the two classical bits for half the tests, although it should not make a difference
+		std::cout << " Measured values for the two qubits: " << classicalBits;
+
+		// how is the whole thing looking before Bob's measurement?
+		std::complex<double> receivedAlpha = qt.getBasisStateAmplitude(classicalBits);
+		std::complex<double> receivedBeta = qt.getBasisStateAmplitude(0x4 | classicalBits);
+		std::cout << "... Teleported state: " << receivedAlpha << "|0> + " << receivedBeta << "|1>" << std::endl;
+
+		if (!approxEqual(alpha, receivedAlpha) || !approxEqual(beta, receivedBeta)) return false;
+	}
+
+	return TeleportTests();
 }
 
 bool SuperdenseCodingTests()
