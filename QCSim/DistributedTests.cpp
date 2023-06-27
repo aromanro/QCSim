42455afae89a77bcd8d2273399d7e894a8b4fd71
Adrian Roman <aroman_ro@yahoo.com>
2023-06-27 22:29:01 +0300

    Added tests with random states for distributed CNOT and teleported CNOT

--- QCSim/DistributedTests.cpp
+++ QCSim/DistributedTests.cpp
@@ -18,7 +18,6 @@ bool distributedCNOTTest()
 	QC::Gates::CNOTGate cnot;
 	QC::QubitRegister reg(2);
 
-	// TODO: maybe instead of using basis states, use random states... but for now this should suffice
 	for (unsigned int ctrlQubit = 0; ctrlQubit <= 1; ++ctrlQubit)
 		for (unsigned int targetQubit = 0; targetQubit <= 1; ++targetQubit)
 		{
@@ -59,6 +58,48 @@ bool distributedCNOTTest()
 			}
 		}
 
+	for (int i = 0; i < 16; ++i)
+	{
+		std::complex<double> alpha(dist_ampl(gen), dist_ampl(gen));
+		std::complex<double> beta(dist_ampl(gen), dist_ampl(gen));
+		std::complex<double> gamma(dist_ampl(gen), dist_ampl(gen));
+		std::complex<double> delta(dist_ampl(gen), dist_ampl(gen));
+
+		distCnot.Clear();
+		distCnot.setRawAmplitude(0, alpha);
+		distCnot.setRawAmplitude(1, beta);
+		distCnot.setRawAmplitude(0x8, gamma);
+		distCnot.setRawAmplitude(0x9, delta);
+		distCnot.Normalize();
+		const unsigned int measurements = distCnot.Execute() << 1;
+
+		reg.Clear();
+		reg.setRawAmplitude(0, alpha);
+		reg.setRawAmplitude(1, beta);
+		reg.setRawAmplitude(2, gamma);
+		reg.setRawAmplitude(3, delta);
+		reg.Normalize();
+		reg.ApplyGate(cnot, 1, 0);
+
+		const std::complex<double> v1 = distCnot.getBasisStateAmplitude(measurements);
+		const std::complex<double> v2 = distCnot.getBasisStateAmplitude(measurements | 1);
+		const std::complex<double> v3 = distCnot.getBasisStateAmplitude(0x8 | measurements);
+		const std::complex<double> v4 = distCnot.getBasisStateAmplitude(0x8 | measurements | 1);
+
+		if (!approxEqual(v1, reg.getBasisStateAmplitude(0)) || !approxEqual(v2, reg.getBasisStateAmplitude(1)) ||
+			!approxEqual(v3, reg.getBasisStateAmplitude(2)) || !approxEqual(v4, reg.getBasisStateAmplitude(3)))
+		{
+			std::cout << "Error: distributed CNOT failed for random state" << std::endl;
+
+			std::cout << "Measured: " << measurements << std::endl << std::endl;
+			reg.displayRegister();
+			std::cout << std::endl << std::endl;
+			distCnot.displayRegister();
+
+			return false;
+		}
+	}
+
 	std::cout << "ok" << std::endl;	
 
 	return true;
@@ -75,7 +116,6 @@ bool teleportedCNOTTest()
 	QC::Gates::CNOTGate cnot;
 	QC::QubitRegister reg(2);
 
-	// TODO: maybe instead of using basis states, use random states... but for now this should suffice
 	for (unsigned int ctrlQubit = 0; ctrlQubit <= 1; ++ctrlQubit)
 		for (unsigned int targetQubit = 0; targetQubit <= 1; ++targetQubit)
 		{
@@ -107,6 +147,48 @@ bool teleportedCNOTTest()
 				}
 			}
 		}
+
+	for (int i = 0; i < 16; ++i)
+	{
+		std::complex<double> alpha(dist_ampl(gen), dist_ampl(gen));
+		std::complex<double> beta(dist_ampl(gen), dist_ampl(gen));
+		std::complex<double> gamma(dist_ampl(gen), dist_ampl(gen));
+		std::complex<double> delta(dist_ampl(gen), dist_ampl(gen));
+
+		teleportedCnot.Clear();
+		teleportedCnot.setRawAmplitude(0, alpha);
+		teleportedCnot.setRawAmplitude(1, beta);
+		teleportedCnot.setRawAmplitude(0x20, gamma);
+		teleportedCnot.setRawAmplitude(0x21, delta);
+		teleportedCnot.Normalize();
+		const unsigned int measurements = teleportedCnot.Execute();
+		
+		reg.Clear();
+		reg.setRawAmplitude(0, alpha);
+		reg.setRawAmplitude(1, beta);
+		reg.setRawAmplitude(2, gamma);
+		reg.setRawAmplitude(3, delta);
+		reg.Normalize();
+		reg.ApplyGate(cnot, 0, 1);
+
+		const std::complex<double> v1 = teleportedCnot.getBasisStateAmplitude(measurements);
+		const std::complex<double> v2 = teleportedCnot.getBasisStateAmplitude(measurements | 0x4);
+		const std::complex<double> v3 = teleportedCnot.getBasisStateAmplitude(measurements | 0x8);
+		const std::complex<double> v4 = teleportedCnot.getBasisStateAmplitude(measurements | 0xC);
+		
+		if (!approxEqual(v1, reg.getBasisStateAmplitude(0)) || !approxEqual(v2, reg.getBasisStateAmplitude(1)) ||
+			!approxEqual(v3, reg.getBasisStateAmplitude(2)) || !approxEqual(v4, reg.getBasisStateAmplitude(3))) 
+		{
+			std::cout << "Error: teleported CNOT failed for random state" << std::endl;
+
+			std::cout << "Measured: " << measurements << std::endl << std::endl;
+			reg.displayRegister();
+			std::cout << std::endl << std::endl;
+			teleportedCnot.displayRegister();
+
+			return false;
+		}
+	}
 
 	std::cout << "ok" << std::endl;
 
