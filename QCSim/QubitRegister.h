965539ff9eb919a18e725319f0f512af247678af
Adrian Roman <aroman_ro@yahoo.com>
2024-02-29 18:42:09 +0200

    Minor changes to constructors of the register

--- QCSim/QubitRegister.h
+++ QCSim/QubitRegister.h
@@ -15,10 +15,10 @@ namespace QC {
 		QubitRegister(size_t N = 3, int addseed = 0)
 			: NrQubits(N), NrBasisStates(1ULL << NrQubits), 
 			registerStorage(VectorClass::Zero(NrBasisStates)),
+			resultsStorage(NrBasisStates),
 			uniformZeroOne(0, 1), recordGates(false)
 		{
 			assert(N > 0);
-			resultsStorage.resize(NrBasisStates);
 
 			const uint64_t timeSeed = std::chrono::high_resolution_clock::now().time_since_epoch().count() + addseed;
 			std::seed_seq seed{ uint32_t(timeSeed & 0xffffffff), uint32_t(timeSeed >> 32) };
@@ -30,11 +30,11 @@ namespace QC {
 		// this is a special constructor, I need it for something in a derived work (closed source)
 		QubitRegister(size_t N, VectorClass& v, int addseed = 0)
 			: NrQubits(N), NrBasisStates(1ULL << NrQubits),
+			resultsStorage(NrBasisStates),
 			uniformZeroOne(0, 1), recordGates(false)
 		{
 			assert(N > 0);
 			registerStorage.swap(v);
-			resultsStorage.resize(NrBasisStates);
 
 			const uint64_t timeSeed = std::chrono::high_resolution_clock::now().time_since_epoch().count() + addseed;
 			std::seed_seq seed{ uint32_t(timeSeed & 0xffffffff), uint32_t(timeSeed >> 32) };
