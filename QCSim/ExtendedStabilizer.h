77706a1d9b77b29cd73446aad241c2a747df0431
Adrian Roman <aroman_ro@yahoo.com>
2026-02-15 12:54:03 +0200

    Minor additions

--- QCSim/ExtendedStabilizer.h
+++ QCSim/ExtendedStabilizer.h
@@ -1,4 +1,10 @@
 #pragma once
+		
+#include <complex>
+#include <random>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
 #include "PauliStringXZ.h"
 
@@ -8,7 +14,12 @@ namespace QC {
 	public:
 		using Signs = std::vector<bool>;
 
-		Frame(size_t nrQubits) : amplitude(1., 0.), signs(nrQubits, std::vector<bool>(nrQubits, false)) {}
+		Frame(size_t nrQubits) : amplitude(1., 0.), signs(nrQubits, std::vector<bool>(nrQubits, false))
+		{
+			stabilizers.resize(nrQubits);
+			for (size_t s = 0; s < nrQubits; ++s)
+				stabilizers[s] = PauliStringXZ(nrQubits);
+		}
 
 		size_t GetNrQubits() const
 		{
@@ -30,13 +41,56 @@ namespace QC {
 	public:
 		ExtendedStabilizer() = delete;
 
-		ExtendedStabilizer(size_t nrQubits)
+		explicit ExtendedStabilizer(size_t nrQubits)
+			: gen(std::random_device{}()), rnd(0.5)
 		{
 			frames.emplace_back(nrQubits);
 			frames.back().SetZDiagonal();
 		}
 
+		size_t GetNrQubits() const
+		{
+			if (frames.empty()) return 0;
+			return frames.front().GetNrQubits();
+		}
+
+		void Reset(size_t nrQubits)
+		{
+			frames.clear();
+			frames.emplace_back(nrQubits);
+			frames.back().SetZDiagonal();
+		}
+
+		void ApplyH(size_t /*qubit*/)
+		{
+			throw std::logic_error("ExtendedStabilizer::ApplyH not implemented yet");
+		}
+
+		void ApplyS(size_t /*qubit*/)
+		{
+			throw std::logic_error("ExtendedStabilizer::ApplyS not implemented yet");
+		}
+
+		void ApplyCX(size_t /*target*/, size_t /*control*/)
+		{
+			throw std::logic_error("ExtendedStabilizer::ApplyCX not implemented yet");
+		}
+
+		bool Measure(size_t /*qubit*/)
+		{
+			throw std::logic_error("ExtendedStabilizer::Measure not implemented yet");
+		}
+
+		double GetQubitProbability(size_t /*qubit*/)
+		{
+			throw std::logic_error("ExtendedStabilizer::GetQubitProbability not implemented yet");
+		}
+
 		std::vector<Frame> frames;
+
+	private:
+		std::mt19937 gen;
+		std::bernoulli_distribution rnd;
 	};
 
 }
