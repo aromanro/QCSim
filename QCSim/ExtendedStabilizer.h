67244e3955de80b9018549f431e11be05841cf8d
Adrian Roman <aroman_ro@yahoo.com>
2026-02-15 10:16:55 +0200

    Added extended stabilizer tests source file to cmake

--- QCSim/ExtendedStabilizer.h
+++ QCSim/ExtendedStabilizer.h
@@ -1,16 +1,42 @@
 #pragma once
 
-#include "Clifford.h"
+#include "PauliStringXZ.h"
 
 namespace QC {
 
 	class Frame {
 	public:
+		using Signs = std::vector<bool>;
+
+		Frame(size_t nrQubits) : amplitude(1., 0.), signs(nrQubits, std::vector<bool>(nrQubits, false)) {}
+
+		size_t GetNrQubits() const
+		{
+			return signs.size();
+		}
+
+		void SetZDiagonal()
+		{
+			for (size_t s = 0; s < GetNrQubits(); ++s)
+				stabilizers[s].Z[s] = true;
+		}
+
+		std::complex<double> amplitude;
+		std::vector<Signs> signs;
+		std::vector<PauliStringXZ> stabilizers;
 	};
 
 	class ExtendedStabilizer {
 	public:
 		ExtendedStabilizer() = delete;
+
+		ExtendedStabilizer(size_t nrQubits)
+		{
+			frames.emplace_back(nrQubits);
+			frames.back().SetZDiagonal();
+		}
+
+		std::vector<Frame> frames;
 	};
 
 }
